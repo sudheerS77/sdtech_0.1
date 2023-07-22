49062790692866bb8e4d3ac97f22f39ec5872f16
Sudheer77 <sudheer.gsk777@gmail.com>
2023-07-22 10:30:57 +0530

    updates

--- components/contact/contactForm.jsx
+++ components/contact/contactForm.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import axios from "axios";
+import PhoneInput from "react-phone-input-2";
 
 import cn from "./contact.module.css";
-import axios from "axios";
+import "react-phone-input-2/lib/bootstrap.css";
+
 
 const ContactForm = () => {
   const [formDetails, setFormDetails] = useState({
@@ -22,9 +25,12 @@ const ContactForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const submitData = async () => {
-      const data = await axios.post("https://sdtech-0-1.vercel.app/api/contact", {
-        data: formDetails,
-      });
+      const data = await axios.post(
+        "https://sdtech-0-1.vercel.app/api/contact",
+        {
+          data: formDetails,
+        }
+      );
     };
     submitData();
   };
@@ -35,7 +41,7 @@ const ContactForm = () => {
         <div className={cn.cnform__container}>
           <div className={`${cn.cn__card} ${cn.form__left}`}>
             <form onSubmit={handleSubmit} method="POST">
-              <h3>CONTACT US</h3>
+              <h3>COURSE ENQUERY</h3>
               <p>Submit your details, You will receive a Call Back!</p>
               <div className={cn.cn__name}>
                 <input
@@ -83,58 +89,77 @@ const ContactForm = () => {
               <button>Submit</button>
             </form>
           </div>
-          <div className={`${cn.form__right}`}>
-            <div className={`${cn.cn__card} ${cn.contact_details}`}>
-              <div>
-                <h4>New Customers</h4>
+          <div className={`${cn.cn__card} ${cn.form__left}`}>
+            <form onSubmit={handleSubmit} method="POST">
+              <h3>B2B ENQUERY</h3>
+              <p>Submit your details, You will receive a Call Back!</p>
+              <div className={cn.cn__name}>
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  name="firstName"
+                  onChange={handleChange}
+                  className={cn.cn_tinput}
+                  required
+                />
+                <input
+                  type="text"
+                  name="lastName"
+                  onChange={handleChange}
+                  placeholder="Organization Name"
+                  className={cn.cn_tinput}
+                  required
+                />
               </div>
               <div>
-                <span>Call Us</span>
-                <span>+91 9658965896</span>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  required
+                  onChange={handleChange}
+                  className={cn.cn_tinput}
+                />
+                {/* <input
+                  type="number"
+                  placeholder="Phone Number"
+                  maxLength={10}
+                  name="phoneNumber"
+                  required
+                  onChange={handleChange}
+                  className={cn.cn_tinput}
+                /> */}
+                <div>
+                  <PhoneInput
+                    // className={crf.phoneNumber}
+                    country={"in"}
+                    // value={phoneNumber}
+                    name="whatsAppNumber"
+                    required="true"
+                    inputProps={{
+                      name: "phone",
+                      required: true,
+                      autoFocus: false,
+                    }}
+                    // onChange={(phone) => {
+                    //   setStudentDetails((prev) => ({
+                    //     ...prev,
+                    //     whatsAppNumber: phone,
+                    //   }));
+                    // }}
+
+                    // onChange={(phone) => setPhoneNumber(phone)}
+                  />
+                </div>
+                <textarea
+                  placeholder="Message"
+                  name="message"
+                  required
+                  onChange={handleChange}
+                ></textarea>
               </div>
-              <div>
-                <span>Whatsapp</span>
-                <span>Click Here To Chat</span>
-              </div>
-              <div>
-                <span>Email</span>
-                <span>info@sdtechacademy.com</span>
-              </div>
-            </div>
-            <div className={`${cn.cn__card} ${cn.contact_details}`}>
-              <div>
-                <h4>New Customers</h4>
-              </div>
-              <div>
-                <span>Call Us</span>
-                <span>+91 9658965896</span>
-              </div>
-              <div>
-                <span>Whatsapp</span>
-                <span>Click Here To Chat</span>
-              </div>
-              <div>
-                <span>Email</span>
-                <span>info@sdtechacademy.com</span>
-              </div>
-            </div>
-            <div className={`${cn.cn__card} ${cn.contact_details}`}>
-              <div>
-                <h4>New Customers</h4>
-              </div>
-              <div>
-                <span>Call Us</span>
-                <span>+91 9658965896</span>
-              </div>
-              <div>
-                <span>Whatsapp</span>
-                <span>Click Here To Chat</span>
-              </div>
-              <div>
-                <span>Email</span>
-                <span>info@sdtechacademy.com</span>
-              </div>
-            </div>
+              <button>Submit</button>
+            </form>
           </div>
         </div>
       </div>
@@ -143,3 +168,56 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
+<>
+  <div className={`${cn.cn__card} ${cn.contact_details}`}>
+    <div>
+      <h4>New Customers</h4>
+    </div>
+    <div>
+      <span>Call Us</span>
+      <span>+91 9658965896</span>
+    </div>
+    <div>
+      <span>Whatsapp</span>
+      <span>Click Here To Chat</span>
+    </div>
+    <div>
+      <span>Email</span>
+      <span>info@sdtechacademy.com</span>
+    </div>
+  </div>
+  <div className={`${cn.cn__card} ${cn.contact_details}`}>
+    <div>
+      <h4>New Customers</h4>
+    </div>
+    <div>
+      <span>Call Us</span>
+      <span>+91 9658965896</span>
+    </div>
+    <div>
+      <span>Whatsapp</span>
+      <span>Click Here To Chat</span>
+    </div>
+    <div>
+      <span>Email</span>
+      <span>info@sdtechacademy.com</span>
+    </div>
+  </div>
+  <div className={`${cn.cn__card} ${cn.contact_details}`}>
+    <div>
+      <h4>New Customers</h4>
+    </div>
+    <div>
+      <span>Call Us</span>
+      <span>+91 9658965896</span>
+    </div>
+    <div>
+      <span>Whatsapp</span>
+      <span>Click Here To Chat</span>
+    </div>
+    <div>
+      <span>Email</span>
+      <span>info@sdtechacademy.com</span>
+    </div>
+  </div>
+</>;
--- components/home/courseCategories.jsx
+++ components/home/courseCategories.jsx
@@ -1,278 +1,194 @@
 import Link from "next/link";
 import React, { useState } from "react";
 
-import {BsCode, BsRobot} from 'react-icons/bs'
-import {SiWebflow} from 'react-icons/si'
+import { BsCode, BsRobot } from "react-icons/bs";
+import { SiWebflow } from "react-icons/si";
 
-import hcc from "./categories.module.css"
+import hcc from "./categories.module.css";
 
 const CourseCategories = () => {
-    const [activeCourse, setActiveCourse] = useState("Programming")
+  const [activeCourse, setActiveCourse] = useState("Programming");
 
-    const categories = [
+  const categories = [
+    {
+      name: "Programming",
+      icon: <BsCode className={hcc.caticon} />,
+      children: [
         {
-            name: "Programming",
-            icon: <BsCode className={hcc.caticon}/>,
-            children: [
-                {
-                    name: "Python",
-                    link: 'python'
-                },
-                {
-                    name: "Java",
-                    link: 'java'
-                },
-                {
-                    name: "JavaScript",
-                    link: 'java-script'
-                },
-                {
-                    name: "C",
-                    link: 'c'
-                },
-                {
-                    name: "C++",
-                    link: 'c++'
-                },
-                {
-                    name: "PHP",
-                    link: 'php'
-                },
-                {
-                    name: "R",
-                    link: 'r'
-                },
-                {
-                    name: "Python",
-                    link: 'python'
-                },
-                {
-                    name: "Java",
-                    link: 'java'
-                },
-                {
-                    name: "JavaScript",
-                    link: 'java-script'
-                },
-                {
-                    name: "C",
-                    link: 'c'
-                },
-                {
-                    name: "C++",
-                    link: 'c++'
-                },
-                {
-                    name: "PHP",
-                    link: 'php'
-                },
-                {
-                    name: "R",
-                    link: 'r'
-                }, {
-                    name: "Python",
-                    link: 'python'
-                },
-                {
-                    name: "Java",
-                    link: 'java'
-                },
-                {
-                    name: "JavaScript",
-                    link: 'java-script'
-                },
-                {
-                    name: "C",
-                    link: 'c'
-                },
-                {
-                    name: "C++",
-                    link: 'c++'
-                },
-                {
-                    name: "PHP",
-                    link: 'php'
-                },
-                {
-                    name: "R",
-                    link: 'r'
-                },
-            ]
+          name: "Python",
+          link: "python",
         },
         {
-            name: "Web Development",
-            icon: <SiWebflow className={hcc.caticon}/>,
-            children: [                
-                {
-                    name: "Python",
-                    link: 'python'
-                },
-                {
-                    name: "Java",
-                    link: 'java'
-                },
-                {
-                    name: "JavaScript",
-                    link: 'java-script'
-                },
-                {
-                    name: "C",
-                    link: 'c'
-                },
-                {
-                    name: "C++",
-                    link: 'c++'
-                },
-                {
-                    name: "PHP",
-                    link: 'php'
-                },
-                {
-                    name: "R",
-                    link: 'r'
-                },
-            ]
+          name: "Java",
+          link: "java",
         },
         {
-            name: "Robotics",
-            icon: <BsRobot className={hcc.caticon}/>,
-            children: [
-                {
-                    name: "ROBOTICS",
-                    link: 'python'
-                },
-                {
-                    name: "Python",
-                    link: 'python'
-                },
-                {
-                    name: "Java",
-                    link: 'java'
-                },
-                {
-                    name: "JavaScript",
-                    link: 'java-script'
-                },
-                {
-                    name: "C",
-                    link: 'c'
-                },
-                {
-                    name: "C++",
-                    link: 'c++'
-                },
-                {
-                    name: "PHP",
-                    link: 'php'
-                },
-                {
-                    name: "R",
-                    link: 'r'
-                },
-            ]
+          name: "JavaScript",
+          link: "java-script",
         },
         {
-            name: "Machine Learning",
-            icon: <BsCode className={hcc.caticon}/>,
-            children: [
-                {
-                    name: "MACHINE LEARNING",
-                    link: 'python'
-                },
-                {
-                    name: "Java",
-                    link: 'java'
-                },
-                {
-                    name: "JavaScript",
-                    link: 'java-script'
-                },
-                {
-                    name: "C",
-                    link: 'c'
-                },
-                {
-                    name: "C++",
-                    link: 'c++'
-                },
-                {
-                    name: "PHP",
-                    link: 'php'
-                },
-                {
-                    name: "R",
-                    link: 'r'
-                },
-            ]
+          name: "GOLANG",
+          link: "golang",
         },
         {
-            name: "Data Analytics",
-            icon: <BsCode className={hcc.caticon}/>,
-            children: [
-                {
-                    name: "Python",
-                    link: 'python'
-                },
-                {
-                    name: "Java",
-                    link: 'java'
-                },
-                {
-                    name: "JavaScript",
-                    link: 'java-script'
-                },
-                {
-                    name: "C",
-                    link: 'c'
-                },
-                {
-                    name: "C++",
-                    link: 'c++'
-                },
-                {
-                    name: "PHP",
-                    link: 'php'
-                },
-                {
-                    name: "R",
-                    link: 'r'
-                },
-            ]
+          name: "PHP",
+          link: "php",
         },
-    ]
-    const coursesList = categories.filter((data) => data.name === activeCourse)
-   
-    return (
-        <>
-            <div className={hcc.category_container} id="courseCategories">
-                <div className={hcc.category_header}>
-                    <h1>Course Categories</h1>
-                </div>
-                <div className={hcc.categories}>
-                    <div className={hcc.catList}>
-                        {
-                            categories.map((cdata, indx) => (
-                                <button key={indx} className={hcc.catbtn} onClick={() => setActiveCourse(cdata.name)}>
-                                    {cdata.icon}{cdata.name}
-                                </button>
-                            ))
-                        }
-                    </div>
-                    <div className={hcc.coursesList}>
-                        {coursesList[0]?.children?.map((courses, indx) => (
-                            <Link href={`/courses/${courses.link}`} key={indx}>
-                                <div className={hcc.courseName}>
-                                    {courses.name}
-                                </div>
-                            </Link>
-                        ))
+        {
+          name: "R",
+          link: "r",
+        },
+        {
+          name: "C",
+          link: "c",
+        },
+        {
+          name: "C++",
+          link: "c++",
+        },
+      ],
+    },
+    {
+      name: "Web Development",
+      icon: <SiWebflow className={hcc.caticon} />,
+      children: [
+        {
+          name: "Full Stack Wev Development",
+          link: "full-stack-web-development",
+        },
+        {
+          name: "HTML CSS JS",
+          link: "html-css-js",
+        },
+        {
+          name: "HTML",
+          link: "html",
+        },
+        {
+          name: "CSS",
+          link: "css",
+        },
+        {
+          name: "JavaScript",
+          link: "javascript",
+        },
+        {
+          name: "NodeJS",
+          link: "node-js",
+        },
+        {
+          name: "ReactJS",
+          link: "react-js",
+        },
+        {
+          name: "NextJS",
+          link: "next-js",
+        },
+        {
+          name: "Mern Stack",
+          link: "mern-stack",
+        },
+        {
+          name: "Mean Stack",
+          link: "mean-stack",
+        },
+        {
+          name: "Frontend",
+          link: "frontend",
+        },
+        {
+          name: "Backend",
+          link: "backend",
+        },
+        {
+          name: "flask",
+          link: "flask",
+        },
+        {
+          name: "Java Full Stack",
+          link: "java-full-stack",
+        },
+        {
+          name: "Python Full Stack",
+          link: "python-full-stack",
+        },
+      ],
+    },
+    {
+      name: "Mobile APP Development",
+      icon: <BsRobot className={hcc.caticon} />,
+      children: [
+        {
+          name: "React Native",
+          link: "react-native",
+        },
+        {
+          name: "Dart",
+          link: "dart",
+        },
+        {
+          name: "FLutter",
+          link: "flutter",
+        },
+      ],
+    },
+    {
+      name: "Robotics",
+      icon: <BsRobot className={hcc.caticon} />,
+      children: [
+        {
+          name: "Arduino",
+          link: "arduino",
+        },
+      ],
+    },
+    {
+      name: "DataBases",
+      icon: <BsRobot className={hcc.caticon} />,
+      children: [
+        {
+          name: "MySql",
+          link: "mysql",
+        },
+        {
+          name: "MongoDB",
+          link: "mongoDB",
+        },
+      ],
+    },
+  ];
+  const coursesList = categories.filter((data) => data.name === activeCourse);
 
-                        }
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={hcc.category_container} id="courseCategories">
+        <div className={hcc.category_header}>
+          <h1>Course Categories</h1>
+        </div>
+        <div className={hcc.categories}>
+          <div className={hcc.catList}>
+            {categories.map((cdata, indx) => (
+              <button
+                key={indx}
+                className={hcc.catbtn}
+                onClick={() => setActiveCourse(cdata.name)}
+              >
+                {cdata.icon}
+                {cdata.name}
+              </button>
+            ))}
+          </div>
+          <div className={hcc.coursesList}>
+            {coursesList[0]?.children?.map((courses, indx) => (
+              <Link href={`/courses/${courses.link}`} key={indx}>
+                <div className={hcc.courseName}>{courses.name}</div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default CourseCategories;
