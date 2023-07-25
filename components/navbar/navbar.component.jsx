f9ac062db452873cb2165b48198f8bd7810a76e3
Sudheer77 <sudheer.gsk777@gmail.com>
2023-07-25 09:33:44 +0530

    updates

--- components/navbar/navbar.component.jsx
+++ components/navbar/navbar.component.jsx
@@ -58,62 +58,69 @@ const Navbar = () => {
         </div>
       </nav> */}
 
-      <nav className={navcss.nav}>
-        <div className={navcss.navbar}>
-          <div className={navcss.navlogo}>
-            <Link href="/">
-              <Image src={logo} alt="SD" className={navcss.img} />
-            </Link>
-            <div className={navcss.hamburger} onClick={handleMobileMenuToggle}>
-              {isMobileMenuOpen ? <RiCloseFill /> : <RiMenu3Fill />}
-            </div>
-            {/* Tech Academy */}
-          </div>
-          <div
-            className={`${navcss.menu}  ${isMobileMenuOpen ? navcss.open : ""}`}
-          >
-            {isMobileMenuOpen ? (
-              <div className={navcss.nav_head}>
-                <div className={navcss.nav_logo}>
-                  <Link href="/">
-                    <Image src={logo} alt="SD" className={navcss.img} />
-                  </Link>
-                  <div
-                    className={navcss.hamburger}
-                    onClick={handleMobileMenuToggle}
-                  >
-                    {isMobileMenuOpen ? <RiCloseFill /> : <RiMenu3Fill />}
-                  </div>
-                </div>
-                {/* Tech Academy */}
+      <nav className={navcss.nav_bar}>
+        <div className={navcss.nav}>
+          <div className={navcss.navbar}>
+            <div className={navcss.navlogo}>
+              <Link href="/">
+                <Image src={logo} alt="SD" className={navcss.img} />
+              </Link>
+              <div
+                className={navcss.hamburger}
+                onClick={handleMobileMenuToggle}
+              >
+                {isMobileMenuOpen ? <RiCloseFill /> : <RiMenu3Fill />}
               </div>
-            ) : (
-              ""
-            )}
-            <div className={navcss.menu_group}>
-              <ul className={""}>
-                <Link href="/">
-                  <li>Home</li>
-                </Link>
-                <Link href="/courses">
-                  <li>Courses</li>
-                </Link>
-                <Link href="/about-us">
-                  <li>About Us</li>
-                </Link>
-                {/* <Link href="#">
+              {/* Tech Academy */}
+            </div>
+            <div
+              className={`${navcss.menu}  ${
+                isMobileMenuOpen ? navcss.open : ""
+              }`}
+            >
+              {isMobileMenuOpen ? (
+                <div className={navcss.nav_head}>
+                  <div className={navcss.nav_logo}>
+                    <Link href="/">
+                      <Image src={logo} alt="SD" className={navcss.img} />
+                    </Link>
+                    <div
+                      className={navcss.hamburger}
+                      onClick={handleMobileMenuToggle}
+                    >
+                      {isMobileMenuOpen ? <RiCloseFill /> : <RiMenu3Fill />}
+                    </div>
+                  </div>
+                  {/* Tech Academy */}
+                </div>
+              ) : (
+                ""
+              )}
+              <div className={navcss.menu_group}>
+                <ul className={""}>
+                  <Link href="/">
+                    <li>Home</li>
+                  </Link>
+                  <Link href="/courses">
+                    <li>Courses</li>
+                  </Link>
+                  <Link href="/about-us">
+                    <li>About Us</li>
+                  </Link>
+                  {/* <Link href="#">
                 <li>Blogs</li>
               </Link> */}
-                <Link href="/contactus">
-                  <li>Contact Us</li>
-                </Link>
-                <Link href="/#faq">
-                  <li>FAQ</li>
-                </Link>
-              </ul>
-              <div className={navcss.btn_group}>
-                <button className={navcss.signin_btn}>SignIn</button>
-                <button className={navcss.signup_btn}>SignUp</button>
+                  <Link href="/contactus">
+                    <li>Contact Us</li>
+                  </Link>
+                  <Link href="/#faq">
+                    <li>FAQ</li>
+                  </Link>
+                </ul>
+                <div className={navcss.btn_group}>
+                  <button className={navcss.signin_btn}>SignIn</button>
+                  <button className={navcss.signup_btn}>SignUp</button>
+                </div>
               </div>
             </div>
           </div>
