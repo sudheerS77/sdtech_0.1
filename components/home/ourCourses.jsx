214352d0c8ae864071b628ba8f3b4db81a5dd33e
Sudheer77 <sudheer.gsk777@gmail.com>
2023-07-27 15:56:59 +0530

    update hero section

--- components/home/ourCourses.jsx
+++ components/home/ourCourses.jsx
@@ -1,72 +1,65 @@
 import React from "react";
 
-import hc from "./ourCourses.module.css"
+import hc from "./ourCourses.module.css";
 import CourseCard from "./courseCard";
 import Link from "next/link";
 
 const OurCourses = () => {
-    const ourCoursesData = [
-        {
-            name: "Full Stack Web Development",
-            image: "https://www.webindiamaster.com/assests/images/web-development-company-1-2022.jpg",
-            cost: 5000,
-            link: "full-stack-web-development"
-        },
-        {
-            name: "Machine Learning BootCamp",
-            image: "https://www.digitalsilk.com/wp-content/uploads/2022/09/website-development-process-1.jpg",
-            cost: 5000,
-            link: "machine-learning-bootcamp"
-        },
-        {
-            name: "Robotics 2.0",
-            image: "https://www.thebigredgroup.com/wp-content/uploads/2021/07/5-reasons.jpg",
-            cost: 5000,
-            link: "robotics-2.0 "
-        },
-        {
-            name: "Machine Learning BootCamp",
-            image: "https://www.digitalsilk.com/wp-content/uploads/2022/09/website-development-process-1.jpg",
-            cost: 5000,
-            link: "machine-learning-bootcamp"
-        },
-        {
-            name: "Robotics 2.0",
-            image: "https://www.thebigredgroup.com/wp-content/uploads/2021/07/5-reasons.jpg",
-            cost: 5000,
-            link: "robotics-2.0 "
-        },
-        {
-            name: "Full Stack Web Development",
-            image: "https://www.webindiamaster.com/assests/images/web-development-company-1-2022.jpg",
-            cost: 5000,
-            link: "full-stack-web-development"
-        },
-        
-       
-    ]
+  const ourCoursesData = [
+    {
+      name: "Python Mastery: Unlock Your Coding Superpowers",
+      image:
+        "https://res.cloudinary.com/df8aswwta/image/upload/v1689850188/images/pamvha9lzu6mkpmobqep.png",
+      cost: 5000,
+      link: "python-programming",
+    },
+    {
+      name: "Mastering JavaScript: From Basics to Advanced",
+      image:
+        "https://res.cloudinary.com/df8aswwta/image/upload/v1689850187/images/enwdonu3ntfbsgpklqht.png",
+      cost: 0,
+      link: "java-script",
+    },
+    {
+      name: "Web Development: Comprehensive Guide to HTML, CSS, and JavaScript",
+      image:
+        "https://res.cloudinary.com/df8aswwta/image/upload/v1690368005/images/h63ykqvog4i4uht9xzg8.png",
+      cost: 0,
+      link: "html-css-js",
+    },
+    {
+      name: "React.js Mastery: Comprehensive Guide to React.js",
+      image:
+        "https://res.cloudinary.com/df8aswwta/image/upload/v1689850185/images/gwz3e2lo0j30se3qqw3v.png",
+      cost: 0,
+      link: "reactjs",
+    },
+    {
+      name: "Complete Java - Full Stack Development",
+      image:
+        "https://res.cloudinary.com/df8aswwta/image/upload/v1689850187/images/u4jzfmfxakcgliqoca0m.png",
+      cost: 0,
+      link: "complete-java-full-stack-development",
+    },
+  ];
 
-    return (
-        <>
-            <div className={hc.oc_container}>
-                <div className={hc.oc_header}>
-                    <h1>Our Courses</h1>
-                    <Link href="/courses" className={hc.oc_view_all}>
-                        <span>
-                            View All
-                        </span>
-                    </Link>
-                </div>
-                <div className={hc.oc_section}>
-                    {
-                        ourCoursesData.map((data, indx) => (
-                            <CourseCard key={indx} data={data} />
-                        ))
-                    }
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={hc.oc_container}>
+        <div className={hc.oc_header}>
+          <h1>Our Courses</h1>
+          <Link href="/courses" className={hc.oc_view_all}>
+            <span>View All</span>
+          </Link>
+        </div>
+        <div className={hc.oc_section}>
+          {ourCoursesData.map((data, indx) => (
+            <CourseCard key={indx} data={data} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default OurCourses;
