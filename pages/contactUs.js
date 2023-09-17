1734bba1050f32061b56af4fa468dccc1a51a955
userName <youremail>
2023-09-17 20:52:09 +0530

    test_0.1

--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,8 +1,24 @@
 import HomeLayout from "@/Layout/Home.layout";
+import HeroBanner from "@/components/Atoms/Banners/heroBanner";
+import ContactForm from "@/components/contact/contactForm";
 import React from "react";
 
 const contactUs = () => {
-  return <HomeLayout>contactUs</HomeLayout>;
+  const bannerData = {
+    title: "Contact Us",
+    header: "Unlock Your Coding Potential with Us Today!",
+    header2: null,
+    description: "Let us transform your coding dreams into reality",
+    poster:
+      "https://cdni.iconscout.com/illustration/premium/thumb/contact-us-3483599-2912016.png https://i.pinimg.com/originals/26/96/b8/2696b872b87bfe565a2ff12f3a1cb7c0.jpg",
+  };
+
+  return (
+    <HomeLayout>
+      <HeroBanner bannerData={bannerData} poster={true} />
+      <ContactForm />
+    </HomeLayout>
+  );
 };
 
 export default contactUs;
