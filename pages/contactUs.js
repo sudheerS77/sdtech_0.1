6e53b62c686e7685573daff8aa3673696d25e991
userName <youremail>
2023-09-17 20:43:02 +0530

    test_0.1

---
+++ pages/contactUs.js
@@ -0,0 +1,8 @@
+import HomeLayout from "@/Layout/Home.layout";
+import React from "react";
+
+const contactUs = () => {
+  return <HomeLayout>contactUs</HomeLayout>;
+};
+
+export default contactUs;
--- pages/roadMaps/index.js
+++ pages/roadMaps/index.js
@@ -11,20 +11,26 @@ const RoadMaps = () => {
     <>
       <HomeLayout>
         {/* <div className={rm.rm_border} /> */}
-        <div className={rm.rm_container}>
-          {/* <h1>
+        <div className={rm.rm_layout1}>
+          <div className={rm.rm_layout2}>
+            <div className={rm.rm_layout3}>
+              <div className={rm.rm_container}>
+                {/* <h1>
           Learning Pathways Hub: <br /> Your Guide to Mastering Full Stack, Data
           Science, Robotics, and More!
         </h1> */}
-          <div className={rm.rm_hero}>
-            <h1>Tech Mastery: Guided Learning Pathways for All</h1>
-            <p>
-              Your path to success starts here. Explore expertly designed
-              learning roadmaps for tech, data, and beyond. Start your journey
-              today.
-            </p>
+                <div className={rm.rm_hero}>
+                  <h1>Tech Mastery: Guided Learning Pathways for All</h1>
+                  <p>
+                    Your path to success starts here. Explore expertly designed
+                    learning roadmaps for tech, data, and beyond. Start your
+                    journey today.
+                  </p>
+                </div>
+                <RoadMapsComponent />
+              </div>
+            </div>
           </div>
-          <RoadMapsComponent />
         </div>
       </HomeLayout>
     </>
