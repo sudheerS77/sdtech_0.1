6a86296585ad6e5626035d2480c2737ed393370f
userName <youremail>
2023-09-21 10:07:56 +0530

    test_0.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 // slick-carousel css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
