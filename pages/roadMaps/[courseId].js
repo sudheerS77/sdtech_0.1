ba65e8252aa78ced0935a12c5e495956240ddb00
userName <youremail>
2023-09-21 12:19:49 +0530

    add roadmaps page placeholder

--- pages/roadMaps/[courseId].js
+++ pages/roadMaps/[courseId].js
@@ -1,7 +1,23 @@
+import HomeLayout from "@/Layout/Home.layout";
 import React from "react";
 
 const IndividualRoadMap = () => {
-  return <div>IndividualRoadMap</div>;
+  const styles = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "600px",
+    fontSize: "2rem",
+    color: "#091520",
+    fontWeight: "700",
+  };
+  return (
+    <HomeLayout>
+      <div style={styles}>
+        Unlock Your Coding Journey: Coming Soon - Comprehensive Roadmaps.
+      </div>
+    </HomeLayout>
+  );
 };
 
 export default IndividualRoadMap;
